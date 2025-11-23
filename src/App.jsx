991178bda1714dc91baa3f04c09793cebbb5d614
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2025-11-23 21:42:15 +0530

    app.jsx/./components/card fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import LandingPage from './components/LandingPage'
 import Marquee from './components/Marquee'
 import About from './components/About'
 import Featured from './components/Featured'
-import Cards from './components/cards'
+import Cards from './components/Cards'
 import Footer from './components/Footer'
 import LocomotiveScroll from 'locomotive-scroll'
 
