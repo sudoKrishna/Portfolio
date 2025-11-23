d379370a37dea18c6d2240cac1f10c1b9a8f3b80
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2025-11-23 22:17:02 +0530

    fix card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,17 +5,17 @@ const Cards = () => {
     <div className='w-full h-screen bg-zinc-100 flex items-center px-32 gap-5'>
         <div className='cardcontainer h-[50vh] w-1/2'>
         <div className='card relative rounded-xl w-full h-full bg-[#004D43] flex items-center justify-center'>
-          <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg"></img>
+          <h1 className='w-32 text-[#CDEA68] text-6xl font-[Neue] mr-6 font-semibold'>PORT</h1>
           <button className='absolute px-5 py-1 rounded-full border-2 left-10 bottom-10'>&copy; hey</button>
         </div>
         </div>
       <div className='cardcontainer flex gap-5 w-1/2 h-[50vh]'>
       <div className='card relative flex items-center justify-center rounded-xl w-1/2 h-full bg-[#192826]'>
-      <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg"></img>
+      <h1 className='w-32 text-[#CDEA68] text-6xl font-[Neue] mr-6 font-semibold'>PORT</h1>
           <button className='absolute px-5 py-1 rounded-full border-2 left-10 bottom-10'>&copy; hey</button>
           </div>
       <div className='card relative flex items-center justify-center rounded-xl w-1/2 h-full bg-[#192826]'>
-      <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg"></img>
+      <h1 className='w-32 text-[#CDEA68] text-6xl font-[Neue] mr-6 font-semibold'>PORT</h1>
           <button className='absolute px-5 py-1 rounded-full border-2 left-10 bottom-10'>&copy; hey</button>
       </div>
       </div>
